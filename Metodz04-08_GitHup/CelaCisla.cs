595b49ccb35b10ce0dac9475d27b9d772448b037
Scitani <havelkaj.04@A321-11>
2022-03-02 11:24:18 +0100

    Soucet lich a Faktorial a yacatek Prvocislo

--- Metodz04-08_GitHup/CelaCisla.cs
+++ Metodz04-08_GitHup/CelaCisla.cs
@@ -29,7 +29,67 @@ namespace Metodz04_08_GitHup
                 }
                 return mocnina;
             }
+
         }
+        public static int Faktorial(int a)
+        {
+            int faktorial = 1;
+            for (int i = 1; i <= a; ++i)
+            {
+                faktorial *= i;
+            }
+            return faktorial;
+        }
+
+
+        public static int CifSoucet(int a)
+        {
+            int Lichcifra = 0;
+
+            while (a > 0)
+            {
+                int cifra = a % 10;
+                
+                if (cifra % 2 != 0)
+                {
+                    Lichcifra += cifra;
+
+                }
+                a = a / 10;
+
+            }
+
+            return Lichcifra;
+
+        }
+        public static bool Prvocislo(int n)
+        {
+            bool jePrvocislo = false;
+            int   m = 0, trip = 0;
+            
+            
+            m = n / 2;
+            for (int i = 2; i <= m; i++)
+            {
+                if (n % i == 0)
+                {
+                    jePrvocislo = false;
+                   trip = 1;
+                    break;
+                }
+            }
+            if (trip == 0)
+            {
+                jePrvocislo = true;
+            }
+                
+
+
+
+
+            return (jePrvocislo);
+        }
+            
 
     }
 }
--- Metodz04-08_GitHup/Form1.cs
+++ Metodz04-08_GitHup/Form1.cs
@@ -27,16 +27,22 @@ namespace Metodz04_08_GitHup
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            int x = int.Parse(textBox3.Text);
+            MessageBox.Show("Faktorial je " + CelaCisla.Faktorial(x));
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
+            int x = int.Parse(textBox4.Text);
+            MessageBox.Show("Soucet lich cifer je " + CelaCisla.CifSoucet(x));
 
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
+            int x = int.Parse(textBox5.Text);
+           
+
 
         }
 
